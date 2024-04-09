12cf1da066eeb0f80fb5880c37e38ffa4cd824d6
Salih Zioui <www.cjb@gmx.de>
2024-04-09 19:44:34 +0200

    adjust test setup to changed vitest configuration (no globals anymore)

--- tests/vitest.setup.ts
+++ tests/vitest.setup.ts
@@ -1,1 +1,12 @@
-import '@testing-library/jest-dom';
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { cleanup } from '@testing-library/react';
+import { afterEach, expect } from 'vitest';
+
+// since we do not use vitest globals we need to extend expect
+expect.extend(matchers);
+
+// clear environment after each test case
+afterEach(() => {
+  cleanup();
+});
+
