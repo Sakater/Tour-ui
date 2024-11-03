eeb400840ae0004e1471d84f4674a7c64b6f0012
Salih Zioui <www.cjb@gmx.de>
2024-11-03 08:04:58 +0100

    chore(TSP-1): finalize component for added nominatim locations

--- src/LocationList.tsx
+++ src/LocationList.tsx
@@ -1,10 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import {
+	Box,
+	IconButton,
+	List,
+	ListItem,
+	ListItemText,
+	Typography,
+} from "@mui/material";
 import { useContext } from "react";
 import { LocationContext } from "./LocationContext";
 import type { LocationContextType } from "./LocationContext";
+import { Delete } from "@mui/icons-material";
 
 export const LocationList = () => {
-	const { locations } = useContext(LocationContext) as LocationContextType;
+	const { locations, removeLocation } = useContext(
+		LocationContext,
+	) as LocationContextType;
 
 	const getLocationListContent = () => {
 		if (locations.length === 0) {
@@ -14,6 +24,41 @@ export const LocationList = () => {
 				</Typography>
 			);
 		}
+
+		return (
+			<List
+				sx={{
+					padding: 0.5,
+					paddingTop: 0,
+					paddingBottom: 0,
+					display: "flex",
+					flexDirection: "column",
+					gap: 1,
+					overflow: "auto",
+					maxHeight: "450px",
+				}}
+			>
+				{locations.map((location) => (
+					<ListItem
+						key={location.id}
+						disablePadding
+						sx={{
+							backgroundColor: "#FFFFFF",
+							padding: 1,
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							gap: 2,
+						}}
+					>
+						<ListItemText>{location.display_name}</ListItemText>
+						<IconButton size="small" onClick={() => removeLocation(location)}>
+							<Delete />
+						</IconButton>
+					</ListItem>
+				))}
+			</List>
+		);
 	};
 
 	return (
